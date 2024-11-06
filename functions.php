c8da01034a78527d9ae13c871248803edc7fbbff
Abel olguin <osmer19@hotmail.com>
2024-11-06 01:39:38 -0600

    Crear action hooks (fixed)

--- functions.php
+++ functions.php
@@ -29,9 +29,18 @@ new DemoData();
 	->call(fn() => \Abolch\App\Helpers::view('card'));
 */
 
-function ejemplo($arg1, $arg2 = null) {
-	die('hola');
-}
-add_action('abolch_save_settings','ejemplo', 10, 2);
 
-remove_action('abolch_save_settings','ejemplo', 10, 2);
+#archivo dentro de un plugin
+function hola1($social, $token){
+	echo 1;
+}
+
+function hola2($social, $token){
+	echo 2;
+}
+
+add_action('abolch_save_settings', 'hola1', 10, 2);
+add_action('abolch_save_settings','hola2', 10, 2);
+
+#fuera del plugin
+remove_action('abolch_save_settings', 'hola1', 10, 2);
